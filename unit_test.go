00e500b6c295ad29d981f0d0e8f2c46e957f43d4
Frank Xu <frank.xu@northbright.com>
2015-06-09 14:34:13 +0800

    Add example outputs

--- unit_test.go
+++ unit_test.go
@@ -17,5 +17,8 @@ func Example() {
 	noTagLog.Printf("print messages without tag")
 
 	// Output:
-	//
+	// 2015/06/09 14:32:59 unit_test.go:14 fnlog_test.Example(): i: print infos
+	// 2015/06/09 14:32:59 unit_test.go:15 fnlog_test.Example(): w: print warnnings
+	// 2015/06/09 14:32:59 unit_test.go:16 fnlog_test.Example(): e: print errors
+	// 2015/06/09 14:32:59 unit_test.go:17 fnlog_test.Example(): print messages without tag
 }
