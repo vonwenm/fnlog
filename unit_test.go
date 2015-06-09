9050c73035c14758d671a8b386318bc2d7307f61
Frank Xu <frank.xu@northbright.com>
2015-06-09 14:50:14 +0800

    Move initialization for noTagLog to init()

--- unit_test.go
+++ unit_test.go
@@ -14,8 +14,6 @@ func Example() {
 	wLog := fnlog.New("w")
 	eLog := fnlog.New("e")
 
-	noTagLog = fnlog.New("")
-
 	iLog.Printf("print infos")
 	wLog.Printf("print warnnings")
 	eLog.Printf("print errors")
@@ -26,4 +24,8 @@ func Example() {
 	// 2015/06/09 14:32:59 unit_test.go:15 fnlog_test.Example(): w: print warnnings
 	// 2015/06/09 14:32:59 unit_test.go:16 fnlog_test.Example(): e: print errors
 	// 2015/06/09 14:32:59 unit_test.go:17 fnlog_test.Example(): print messages without tag
+}
+
+func init() {
+	noTagLog = fnlog.New("")
 }
